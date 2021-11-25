98d3c7197a2a42e3f1efe0cb2175baf05ad07e08
Ubuntu <lsj010221@naver.com>
2021-11-25 06:15:22 +0000

    20211125

--- BTT.py
+++ BTT.py
@@ -1,92 +1,62 @@
 import cv2
 import numpy as np
+import RPi.GPIO as GPIO
 
-img = cv2.imread("data/dots1.jpg")
-result = img.copy()
-canny = cv2.Canny(img,50,170)
-cv2.imwrite('output/01_canny.jpg',canny)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16, GPIO.OUT)  # led pin right line, 3rd from bottom
+GPIO.setup(20, GPIO.IN)   # led pin right line, 2nd from bottom
+GPIO.setup(21, GPIO.IN)   # led pin right line, 1st from bottom
 
+cam = cv2.VideoCapture(0)
 
-contours, hierarchy = cv2.findContours(canny,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(img,contours,-1,(0,255,0),2)
-select = np.zeros(canny.shape,np.uint8)
-cv2.drawContours(select,contours,-1,(255),2)
-
-cv2.imwrite('output/02_contours.jpg',select)
-
-contours, hierarchy = cv2.findContours(select,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-select = np.zeros(canny.shape,np.uint8)
-dots = np.zeros(canny.shape,np.uint8)
-
-dot_list = []
-
-for contour in contours :
-    x,y,w,h = cv2.boundingRect(contour)
-    if 3<w<35 and 3<h<35:
-        cv2.drawContours(select,[contour],-1,(255),1)
-        x = int(w*0.5+x)
-        y = int(h*0.5+y)
-        result = cv2.circle(result,(x,y),5,(0,0,255),-1)
-        dots = cv2.circle(dots,(x,y),5,(255),-1)
-        dot_list.append((x,y))
-
-cv2.imwrite('output/03_select.jpg',select)
-
-degree = cv2.minAreaRect(np.array(dot_list))[2]
-if degree>80 :
-    degree = degree-90
-    print(degree)
-    h,w = dots.shape
-    dots = cv2.warpAffine(dots,cv2.getRotationMatrix2D((h/2,w/2),degree,1),(w,h))
-    result = cv2.warpAffine(result,cv2.getRotationMatrix2D((h/2,w/2),degree,1),(w,h))
-elif degree<10 :
-    print(degree)
-    h,w = dots.shape
-    dots = cv2.warpAffine(dots,cv2.getRotationMatrix2D((h/2,w/2),degree,1),(w,h))
-    result = cv2.warpAffine(result,cv2.getRotationMatrix2D((h/2,w/2),degree,1),(w,h))
-else :
-    print(degree)
-
-cv2.imwrite('output/result.jpg',result)
-
-i = 0
-flag = False
-row = []
-while i < dots.shape[0] :
-    if not flag and dots[i].sum() :
-        j = i
-        flag = True
-    elif flag and dots[i].sum() == 0:
-        row.append(int((i+j)*0.5))
-        flag = False
-    i += 1
-
-i = 0
-flag = False
-column = []
-while i < dots.shape[1] :
-    if not flag and dots[:,i].sum() :
-        j = i
-        flag = True
-    elif flag and dots[:,i].sum() == 0:
-        column.append(int((i+j)*0.5))
-        flag = False
-    i += 1
-
-print(row)
-print(column)
-
-dots_line = dots.copy()
-
-for i in row :
-    dots_line[i,:]=255
-    print('row', i)
-for i in column :
-    dots_line[:,i]=255
-    print('col', i)
-
-for i in range(len(row)%3) :
-    r = (row[2] - row[0])*0.5
+flag_power = False
 
 
-cv2.imwrite('output/dots.jpg',dots_line)
+
+
+
+def img_processing(img = np.zeros((480,640,3),np.uint8)) :
+    cv2.imwrite('output/00_frame.jpg',img)
+    
+    img = cv2.Canny(img,50,170)
+    cv2.imwrite('output/01_canny.jpg',img)
+
+    contours, hierarchy = cv2.findContours(img,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    img = np.zeros((480,640),np.uint8)
+    cv2.drawContours(img,contours,-1,(255),2)
+    cv2.imwrite('output/02_contours.jpg',img)
+
+
+
+
+
+print("start")
+
+try:
+    while 1:
+        if GPIO.input(21):
+            while GPIO.input(21) : pass
+            flag_power = not flag_power
+            print("power state :",flag_power)
+            cam = cv2.VideoCapture(0)
+            cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+            cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+        if flag_power:
+            GPIO.output(16,1)
+            ret, frame = cam.read()
+            if GPIO.input(20):
+                print("capture")
+                img_processing(frame)
+                while(GPIO.input(20)) : pass
+        else :
+            GPIO.output(16,0)
+            cam.release()
+            if GPIO.input(20) :
+                break
+finally:
+    print("cleanup")
+    cam.release()
+    GPIO.cleanup()
+
+print("end")
---
+++ BTT_notOPT.py
@@ -0,0 +1,92 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("data/dots1.jpg")
+result = img.copy()
+canny = cv2.Canny(img,50,170)
+cv2.imwrite('output/01_canny.jpg',canny)
+
+
+contours, hierarchy = cv2.findContours(canny,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(img,contours,-1,(0,255,0),2)
+select = np.zeros(canny.shape,np.uint8)
+cv2.drawContours(select,contours,-1,(255),2)
+
+cv2.imwrite('output/02_contours.jpg',select)
+
+contours, hierarchy = cv2.findContours(select,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+select = np.zeros(canny.shape,np.uint8)
+dots = np.zeros(canny.shape,np.uint8)
+
+dot_list = []
+
+for contour in contours :
+    x,y,w,h = cv2.boundingRect(contour)
+    if 3<w<35 and 3<h<35:
+        cv2.drawContours(select,[contour],-1,(255),1)
+        x = int(w*0.5+x)
+        y = int(h*0.5+y)
+        result = cv2.circle(result,(x,y),5,(0,0,255),-1)
+        dots = cv2.circle(dots,(x,y),5,(255),-1)
+        dot_list.append((x,y))
+
+cv2.imwrite('output/03_select.jpg',select)
+
+degree = cv2.minAreaRect(np.array(dot_list))[2]
+if degree>80 :
+    degree = degree-90
+    print(degree)
+    h,w = dots.shape
+    dots = cv2.warpAffine(dots,cv2.getRotationMatrix2D((h/2,w/2),degree,1),(w,h))
+    result = cv2.warpAffine(result,cv2.getRotationMatrix2D((h/2,w/2),degree,1),(w,h))
+elif degree<10 :
+    print(degree)
+    h,w = dots.shape
+    dots = cv2.warpAffine(dots,cv2.getRotationMatrix2D((h/2,w/2),degree,1),(w,h))
+    result = cv2.warpAffine(result,cv2.getRotationMatrix2D((h/2,w/2),degree,1),(w,h))
+else :
+    print(degree)
+
+cv2.imwrite('output/result.jpg',result)
+
+i = 0
+flag = False
+row = []
+while i < dots.shape[0] :
+    if not flag and dots[i].sum() :
+        j = i
+        flag = True
+    elif flag and dots[i].sum() == 0:
+        row.append(int((i+j)*0.5))
+        flag = False
+    i += 1
+
+i = 0
+flag = False
+column = []
+while i < dots.shape[1] :
+    if not flag and dots[:,i].sum() :
+        j = i
+        flag = True
+    elif flag and dots[:,i].sum() == 0:
+        column.append(int((i+j)*0.5))
+        flag = False
+    i += 1
+
+print(row)
+print(column)
+
+dots_line = dots.copy()
+
+for i in row :
+    dots_line[i,:]=255
+    print('row', i)
+for i in column :
+    dots_line[:,i]=255
+    print('col', i)
+
+for i in range(len(row)%3) :
+    r = (row[2] - row[0])*0.5
+
+
+cv2.imwrite('output/dots.jpg',dots_line)
